7f6ba3605005c5151bf3ac9b534d30143d8f2a18
Kenji Greene <kenjigrr@gmail.com>
2020-02-14 12:31:21 -0500

    finished component and started then

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,39 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const makeCard = response => {
+    const newCard = document.createElement('div'),
+          headline = document.createElement('div'),
+          author = document.createElement('div'),
+          img = document.createElement('div'),
+          imgsrc = document.createElement('img'),
+          by = document.createElement('span');
+    
+    newCard.classList.add('card');
+    headline.classList.add('headline');
+    author.classList.add('author');
+    img.classList.add('img-container');
+    
+    newCard.append(headline);
+    newCard.append(author);
+    author.append(img);
+    img.append(imgsrc);
+    author.append(by);
+    
+    headline.textContent = response.headline;
+    imgsrc.setAttribute('src', response.authorPhoto);
+    by.textContent = `By ${response.authorName}`;
+    
+    return newCard;
+}
+axios.get("https://lambda-times-backend.herokuapp.com/articles")
+
+.then(response => {
+    console.log(response);
+    let cardContainer = document.querySelector('.cards-container');
+
+})
+.catch(error =>{
+    console.log(error);
+});
